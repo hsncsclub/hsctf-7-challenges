3e62c874f22de3387346ad4211d0ba3b5e9add17
Daniel Wang <hello@danielwang.dev>
2020-06-01 05:35:25 -0400

    fix(web): fix assets

---
+++ web/debt-simulator/frontend/build/precache-manifest.94bdd174e7879d8ab62372730a6f76d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16abef3b789c83bcf9d2fabc28a0e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7c5fde924e920197b9f",
+    "url": "/static/css/main.6279183e.chunk.css"
+  },
+  {
+    "revision": "aa0d9690536c6e610045",
+    "url": "/static/js/2.f3855285.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3855285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7c5fde924e920197b9f",
+    "url": "/static/js/main.1b8f0187.chunk.js"
+  },
+  {
+    "revision": "af1f14185cf3cb31501f",
+    "url": "/static/js/runtime-main.9368276a.js"
+  }
+]);
--- web/debt-simulator/frontend/build/service-worker.js
+++ web/debt-simulator/frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/debt-simulator/precache-manifest.d75ca637d70ccfced62520785135484c.js"
+  "/precache-manifest.94bdd174e7879d8ab62372730a6f76d7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/debt-simulator/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
